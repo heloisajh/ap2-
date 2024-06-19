8ea748530e8cc7f3acdd1a7b718ecc91667f62d1
heloisajh <124626792+heloisajh@users.noreply.github.com>
2024-06-19 17:23:55 -0300

    Add files via upload

--- somador.vhd
+++ somador.vhd
@@ -3,15 +3,16 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY somador IS
-	GENERIC (
-		N : INTEGER);
-	PORT (
-		add1 : IN signed(N-1 DOWNTO 0);
-		add2 : IN signed(N-1 DOWNTO 0);
-		sum : OUT signed(N DOWNTO 0));
+GENERIC (N : INTEGER);
+PORT (
+	add1 : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+	add2 : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+	sum : OUT STD_LOGIC_VECTOR(N DOWNTO 0));
 END somador;
 
 ARCHITECTURE rtl OF somador IS
+    signal temp_sum: unsigned(N downto 0);
 BEGIN
-	sum <= resize(add1, N + 1) + resize(add2, N + 1);
+	temp_sum <= resize(unsigned(add1), N + 1) + resize(unsigned(add2), N + 1);
+	sum <= std_logic_vector(temp_sum);
 END rtl;
